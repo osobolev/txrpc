fd19481e834b77d958829f258a7d4b9eb927ae46
Oleg Sobolev <sobolev_ov@mail.ru>
2024-10-31 09:32:46 +0100

    Refactor: move same call to httpInvoke

--- modules/body-client/src/txrpc/remote/client/body/BaseClientBodyInteraction.java
+++ modules/body-client/src/txrpc/remote/client/body/BaseClientBodyInteraction.java
@@ -17,23 +17,11 @@ public abstract class BaseClientBodyInteraction implements TxRpcInteraction<ICli
         this.client = client;
     }
 
-    private Either<Object> httpInvoke(Class<?> retType, IClientSessionId sessionId,
-                                      HttpCommand command, HttpId id, Class<? extends IDBCommon> iface, String method,
-                                      Class<?>[] paramTypes, Object[] params) throws IOException {
-        HttpRequest request = new HttpRequest(id, command, iface, method, paramTypes, params);
-        HttpResult result = client.call(retType, sessionId, request);
-        Throwable error = result.error;
-        if (error != null) {
-            return serverException(error);
-        } else {
-            return Either.ok(result.result);
-        }
-    }
+    protected abstract IClientSessionId newSessionId();
 
-    @SuppressWarnings("unchecked")
-    private <T> Either<T> httpInvoke(Class<T> retType, IClientSessionId sessionId, HttpCommand command, HttpId id, Object... params) throws IOException {
-        return (Either<T>) httpInvoke(retType, sessionId, command, id, null, null, null, params);
-    }
+    protected abstract IClientSessionId newSessionId(IClientSessionId sessionId, HttpDBInterfaceInfo info);
+
+    protected abstract HttpId id(IClientSessionId sessionId, String transactionId);
 
     private static Either<Object> serverException(Throwable error) {
         StackTraceElement[] serverST = error.getStackTrace();
@@ -45,50 +33,61 @@ public abstract class BaseClientBodyInteraction implements TxRpcInteraction<ICli
         return Either.error(error);
     }
 
-    protected abstract IClientSessionId newSessionId();
+    private Either<Object> httpInvoke(Class<?> retType, IClientSessionId sessionId, String transactionId,
+                                      HttpCommand command, Class<? extends IDBCommon> iface, String method,
+                                      Class<?>[] paramTypes, Object[] params) throws IOException {
+        HttpId id = id(sessionId, transactionId);
+        HttpRequest request = new HttpRequest(id, command, iface, method, paramTypes, params);
+        HttpResult result = client.call(retType, sessionId, request);
+        Throwable error = result.error;
+        if (error != null) {
+            return serverException(error);
+        } else {
+            return Either.ok(result.result);
+        }
+    }
 
-    protected abstract IClientSessionId newSessionId(IClientSessionId sessionId, HttpDBInterfaceInfo info);
-
-    protected abstract HttpId id(IClientSessionId sessionId, String transactionId);
+    @SuppressWarnings("unchecked")
+    private <T> Either<T> httpInvoke(Class<T> retType, IClientSessionId sessionId, String transactionId,
+                                     HttpCommand command, Object... params) throws IOException {
+        return (Either<T>) httpInvoke(retType, sessionId, transactionId, command, null, null, null, params);
+    }
 
     @Override
     public final Either<NewSession<IClientSessionId>> open(String user, String password) throws IOException {
-        HttpId id = new HttpId();
         IClientSessionId sessionId = newSessionId();
-        return httpInvoke(HttpDBInterfaceInfo.class, sessionId, HttpCommand.OPEN, id, user, password)
+        return httpInvoke(HttpDBInterfaceInfo.class, sessionId, null, HttpCommand.OPEN, user, password)
             .map(info -> new NewSession<>(newSessionId(sessionId, info), info.userObject));
     }
 
     @Override
     public final Either<String> beginTransaction(IClientSessionId sessionId) throws IOException {
-        HttpId id = id(sessionId, null);
-        return httpInvoke(HttpId.class, sessionId, HttpCommand.GET_TRANSACTION, id)
+        return httpInvoke(HttpId.class, sessionId, null, HttpCommand.GET_TRANSACTION)
             .map(tid -> tid.transactionId.toString());
     }
 
     @Override
     public final Either<Void> endTransaction(IClientSessionId sessionId, String transactionId, boolean commit) throws IOException {
-        HttpId id = id(sessionId, transactionId);
-        return httpInvoke(void.class, sessionId, commit ? HttpCommand.COMMIT : HttpCommand.ROLLBACK, id);
+        return httpInvoke(void.class, sessionId, transactionId, commit ? HttpCommand.COMMIT : HttpCommand.ROLLBACK);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public final Either<Object> invoke(IClientSessionId sessionId, String transactionId, Method method, Object[] args) throws IOException {
-        HttpId id = id(sessionId, transactionId);
         Class<? extends IDBCommon> iface = (Class<? extends IDBCommon>) method.getDeclaringClass();
-        return httpInvoke(method.getReturnType(), sessionId, HttpCommand.INVOKE, id, iface, method.getName(), method.getParameterTypes(), args);
+        return httpInvoke(
+            method.getReturnType(), sessionId, transactionId, HttpCommand.INVOKE,
+            iface, method.getName(), method.getParameterTypes(), args
+        );
     }
 
     @Override
     public final Either<Void> ping(IClientSessionId sessionId) throws IOException {
-        HttpId id = id(sessionId, null);
-        return httpInvoke(void.class, sessionId, HttpCommand.PING, id);
+        return httpInvoke(void.class, sessionId, null, HttpCommand.PING);
     }
 
     @Override
     public final Either<Void> close(IClientSessionId sessionId) throws IOException {
-        HttpId id = id(sessionId, null);
-        return httpInvoke(void.class, sessionId, HttpCommand.CLOSE, id);
+        return httpInvoke(void.class, sessionId, null, HttpCommand.CLOSE);
     }
 }
