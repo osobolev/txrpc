5959ccaa2e612cbe2dcbb0e5849f6e39bc25378b
Oleg Sobolev <sobolev_ov@mail.ru>
2024-10-31 09:39:45 +0100

    Refactor: removed unused constructor

--- modules/body-common/src/txrpc/remote/common/body/HttpId.java
+++ modules/body-common/src/txrpc/remote/common/body/HttpId.java
@@ -7,11 +7,6 @@ public final class HttpId implements Serializable {
     public final String sessionId;
     public final Integer transactionId;
 
-    public HttpId() {
-        this.sessionId = null;
-        this.transactionId = null;
-    }
-
     public HttpId(String sessionId, Integer transactionId) {
         this.sessionId = sessionId;
         this.transactionId = transactionId;
