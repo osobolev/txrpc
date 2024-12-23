2c06ee7601c9a5b210fdab1f8651f9a1125b61f3
Oleg Sobolev <sobolev_ov@mail.ru>
2024-12-23 05:15:58 +0100

    Version 2.3

--- buildSrc/src/main/kotlin/lib.gradle.kts
+++ buildSrc/src/main/kotlin/lib.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "io.github.osobolev.txrpc"
-version = "2.2"
+version = "2.3"
 
 (publishing.publications["mavenJava"] as MavenPublication).pom {
     name.set("${project.group}:${project.name}")
