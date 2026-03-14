ad8d6562f9db03dedcca4e7db27c9e9828f5a646
Oleg Sobolev <sobolev_ov@mail.ru>
2026-03-14 12:00:12 +0100

    Allow major updates by default

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ fun getMajor(version: String, majorDepth: Int): String {
 }
 
 fun getMajorDepth(mod: ModuleComponentIdentifier): Int {
-    return 1
+    return 0
 }
 
 tasks.withType(com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask::class).configureEach {
